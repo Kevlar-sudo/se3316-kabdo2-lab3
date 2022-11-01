067d283dd80b406b158ceae02e0566b0b4a25389
kevlar-sudo <81199331+Kevlar-sudo@users.noreply.github.com>
2022-11-01 14:51:55 -0400

    Backend Functionality 2+3
    Completed requirements for backend questions 2 and 3

--- app.js
+++ app.js
@@ -21,11 +21,11 @@ app.use((req,res,next) =>{ //for all routes
 
 router.use(express.json());
 
-//Routes for /api/albums
-router.route('/') //all the routes to the base prefix
-    //get list of albums
+//Routes for /api/tracks
+router.route('/tracks/') //all the routes to the tracks
+   
     
-//post req
+//post req we can use this later for adding songs to the playlist (it worked for album before)
 .post((req,res)=>{
     try{
         const {album_comments,album_date_created,album_date_released,album_engineer} = req.body;
@@ -49,9 +49,9 @@ router.route('/') //all the routes to the base prefix
     }
 })
 
-//get request
+//get request we want to limit it to only the first 30 tracks not to overload insomnia
 .get((req,res)=>{
-    sql = "SELECT * FROM albums";
+    sql = "SELECT * FROM tracks LIMIT 0,30";
     try{
         // const queryObject = url.parse(req.url, true).query; // query paramaters grabbing
         // if(queryObject.field && queryObject.type) 
@@ -75,12 +75,12 @@ router.route('/') //all the routes to the base prefix
     }
 })
 
-router.route('/:album_id') //all routes with specified album_id
+router.route('/tracks/:track_id') //all routes with specified track_id
 
 //Get details of a specific album (THIS NOW WORKS!!!)
 .get((req,res) =>{
-    sql = `SELECT * FROM albums WHERE album_id = ${parseInt(req.params.album_id)}`;
-    console.log(`We are looking for album ${parseInt(req.params.album_id)}`)
+    sql = `SELECT * FROM tracks WHERE track_id = ${parseInt(req.params.track_id)}`;
+    console.log(`We are looking for track ${parseInt(req.params.track_id)}`)
     try{
         db.all(sql,[],(err,rows)=>{
             if (err) 
@@ -98,50 +98,66 @@ router.route('/:album_id') //all routes with specified album_id
             success:false,
         });
     }
-})//FROM THIS POINT FORWARD I havent reviewed what I have
-//create/replace album data for a given id
-.put((req,res) =>{
-    const newalbum = req.body;
-    console.log("Part: ",newalbum);
-    
-    //add id field
-    newalbum.album_id = parseInt(req.params.album_id);
+});
 
-    //replace part with new one
-    const part = albums.findIndex(p => p.album_id === parseInt(newalbum.album_id));
-    if(part <0 ){
-    console.log('Creating new part');
-    albums.push(newalbum);
-    }
-    else{
-        console.log('Modifying part ',req.params.album_id);
-        albums[part] = newalbum;
-    }
-    res.send(newalbum);
+//Getting artist info with a given artist_id
+router.route('/artists/:artist_id') //all routes with specified artist_id
 
+//Get details of a specific artist (THIS NOW WORKS!!!)
+.get((req,res) =>{
+    sql = `SELECT * FROM artists WHERE artist_id = ${parseInt(req.params.artist_id)}`;
+    console.log(`We are looking for artist ${parseInt(req.params.artist_id)}`)
+    try{
+        db.all(sql,[],(err,rows)=>{
+            if (err) 
+            return res.json({ status: 300, success: false, error: err});
+
+            if(rows.length<1) 
+            return res.json({ status: 300, success: false, error: "No match"});
+
+            return res.json({ status:200, data: rows, success: true}); //if match is found
+        });
+
+    }catch (error){ //if some client side error occurs
+        return res.json({
+            status: 400,
+            success:false,
+        });
+    }
 })
-//update plays level
-.post((req,res) =>{
-    const newalbum = req.body;
-    console.log("Part: ",newalbum);
-    
-    
-    //find the album
-    const album = albums.findIndex(p => p.album_id === parseInt(req.params.album_id));
-    if(part <0 ){//not dound
-    res.status(404).send(`Album ${req.params.album_id} not found`);
-    }
-    else{
-        console.log('changing plays for ',req.params.album_id);
-        albums[album].plays += parseInt(req.body.plays); //plays property must exist
-        res.send(albums[album]);
-    }    
 
+//get artist info without specifiying an id
+router.route('/artists/')
+//get request we want to limit it to only the first 30 tracks not to overload insomnia
+.get((req,res)=>{
+    sql = "SELECT * FROM artists LIMIT 0,30";
+    try{
+        // const queryObject = url.parse(req.url, true).query; // query paramaters grabbing
+        // if(queryObject.field && queryObject.type) 
+        //     sql += `WHERE ${queryObject.field} LIKE '%${queryObject.type}%'`;
+        db.all(sql,[],(err,rows)=>{
+            if (err) 
+            return res.json({ status: 300, success: false, error: err});
+
+            if(rows.length<1) 
+            return res.json({ status: 300, success: false, error: "No match"});
+
+            return res.json({ status:200, data: rows, success: true});
+        });
+
+    }catch (error){
+        return res.json({
+            status: 400,
+            success:false,
+        });
+
+    }
 })
+
 
 
 //install the router at /api/albums
-app.use('/api/albums',router)
+app.use('/api',router)
 
 app.listen(3000);
 console.log("Listening on port 3000")
