8cf76e52ad9e1a3377bfe812e604c9a44f4d532a
kevlar-sudo <81199331+Kevlar-sudo@users.noreply.github.com>
2022-11-08 12:07:23 -0500

    Code Cleanup

--- static/script.js
+++ static/script.js
@@ -1,22 +1,16 @@
 //this is the client side script that will hendle displaying the data
 //for the tracks search bars + buttons
 const searchBarTracks = document.getElementById("searchBarTracks");
-const searchBarTracksId = document.getElementById("searchBarTracksId");
 const searchButtonTracks = document.getElementById("searchButtonTracks");
-const searchButtonTracksId = document.getElementById("searchButtonTracksId");
 
 //for the artist search bars + buttons
 const searchBarArtist = document.getElementById("searchBarArtist");
-const searchBarArtistId = document.getElementById("searchBarArtistId");
 const searchButtonArtist = document.getElementById("searchButtonArtist");
-const searchButtonArtistId = document.getElementById("searchButtonArtistId");
 
 //for playlist
 const addPlaylist = document.getElementById("addPlaylist");
 const deleteList = document.getElementById("deleteList");
 
-
-searchButtonArtistId.addEventListener('click',searchArtistId);
 addPlaylist.addEventListener('click',createPlaylist);
 deleteList.addEventListener('click',deletePlaylist)
 
@@ -26,29 +20,6 @@ document.getElementById("addTrack").addEventListener('click',addTrack);
 var playListTracks = {};
 var durations = {};
 
-
-//THIS WORKS lol but we need to format
-function searchArtistId(){
-    let input = searchBarArtistId.value;
-    console.log(input);
-    fetch("/api/artists/"+input,{
-        method: 'GET',
-        
-    })
-    .then(res =>res.json()
-    .then(data => {
-        console.log(data);
-        const l = document.getElementById('inventory');
-        
-        const item = document.createElement('li');
-        
-        
-        item.appendChild(document.createTextNode("id: "+data.data[0].artist_id+"\n,handle: "+data.data[0].artist_handle+"\n,Date Created: "+data.data[0].artist_date_created+"\n,Contact: "+ data.data[0].artist_contact+"\n,Associated Label: "+data.data[0].artist_associated_labels+"\n,Active Year End: "+data.data[0].artist_active_year_end+"\n,Active Year Begin: "+data.data[0].artist_active_year_begin));
-        l.appendChild(item);
-        
-    })
-    )
-};
 //create new playlist front end WORKING
 function createPlaylist(){
     const newList={
@@ -395,7 +366,7 @@ function sortListArtist() {
   //the front end for query searches
   function searchArtistName(){
     let input = searchBarArtist.value;
-    if(/^[A-Za-z0-9_]*$/.test(input) == false){
+    if(/^[A-Za-z0-9_ ]*$/.test(input) == false){
       alert("Please only include Alphanumeric characters!");
       return;
     }
@@ -462,7 +433,7 @@ searchButtonTracks.addEventListener('click',searchTrackName);
   //the front end for query searches
   function searchTrackName(){
     let input = searchBarTracks.value;
-    if(/^[A-Za-z0-9_]*$/.test(input) == false){
+    if(/^[A-Za-z0-9_ ]*$/.test(input) == false){
       alert("Please only include Alphanumeric characters!");
       return;
     }
