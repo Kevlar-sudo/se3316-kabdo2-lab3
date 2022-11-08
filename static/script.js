3c7fc5bc2081ae331411493743c5f59836b5b4c9
kevlar-sudo <81199331+Kevlar-sudo@users.noreply.github.com>
2022-11-07 20:47:53 -0500

    Sorting by artist available

--- static/script.js
+++ static/script.js
@@ -236,4 +236,45 @@ function toSeconds(time) {
   function sum(a, b) {
     return a + b;
   }
+
+document.getElementById("sortArtist").addEventListener('click',sortListArtist);
+
+
+
+
+  //here will go sorter function :O
+function sortListArtist() {
+    var list, i, switching, b, shouldSwitch;
+    list = document.getElementById("listTracks");
+    switching = true;
+    /* Make a loop that will continue until
+    no switching has been done: */
+    while (switching) {
+      // Start by saying: no switching is done:
+      switching = false;
+      b = list.getElementsByTagName("LI");
+      // Loop through all list items:
+      for (i = 0; i < (b.length - 1); i++) {
+        // Start by saying there should be no switching:
+        shouldSwitch = false;
+        /* Check if the next item should
+        switch place with the current item: */
+
+        //to get just the artist
+        console.log(b[i].innerText.toLowerCase().split("artist: ")[1].split(",")[0]);
+        if (b[i].innerText.toLowerCase().split("artist: ")[1].split(",")[0] > b[i + 1].innerText.toLowerCase().split("artist: ")[1].split(",")[0]) {
+          /* If next item is alphabetically lower than current item,
+          mark as a switch and break the loop: */
+          shouldSwitch = true;
+          break;
+        }
+      }
+      if (shouldSwitch) {
+        /* If a switch has been marked, make the switch
+        and mark the switch as done: */
+        b[i].parentNode.insertBefore(b[i + 1], b[i]);
+        switching = true;
+      }
+    }
+  }
   
